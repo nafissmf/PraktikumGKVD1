a9fa3203e02ef895a59fdfec6db47df53e2561f6
Alcendol <117699108+Alcendol@users.noreply.github.com>
2024-02-27 07:58:21 +0700

    Update mainPrak1.cpp

--- Praktikum 1/24060122130081/mainPrak1.cpp
+++ Praktikum 1/24060122130081/mainPrak1.cpp
@@ -5,6 +5,83 @@
 #include <GL/glut.h>
 #include <math.h>
 
+void Lines(void){
+	//GL_POINT, garis menggunakan GL_LINES, buatlah program yang memanfaatkan fungsi GL_LINE_STRIP, GL_LINE_LOOP,
+	//GL_TRIANGLE_FAN, GL_TRIANGLE_STRIP, GL_QUADS, dan GL_QUAD_STRIP.
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POINTS);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(0.5f,0.5f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.3f,0.3f,0.0f);
+	glEnd();
+	
+	glBegin(GL_LINES);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(0.4f,0.4f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.2f,0.2f,0.0f);
+	glEnd();
+
+	glBegin(GL_LINE_STRIP);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(0.55f,0.55f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.65f,0.65f,0.0f);
+	glEnd();
+		 
+	glBegin(GL_LINE_LOOP);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(0.6f,0.2f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.9f,0.2f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.65f,0.3f,0.0f);
+	glEnd();
+
+	glBegin(GL_TRIANGLE_FAN);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(0.1f,0.1f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.3f,0.1f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.15f,0.3f,0.0f);
+	glEnd();
+
+	glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(0.35f,0.15f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.60f,0.15f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(0.45f,0.35f,0.0f);
+	glEnd();
+	
+	glBegin(GL_QUADS);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-0.1f,-0.1f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(-0.3f,-0.1f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(-0.3f,-0.2f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(-0.1f,-0.2f,0.0f);
+	glEnd();
+
+	glBegin(GL_QUAD_STRIP);
+		glColor3f(1.0f,1.0f,0.0f);
+		glVertex3f(-0.3f,-0.5f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(-0.9f,-0.5f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(-0.9f,-0.7f,0.0f);
+		glColor3f(0.0f,1.0f,1.0f);
+		glVertex3f(-0.3f,-0.7f,0.0f);
+	glEnd();
+
+	glFlush();
+}
+
 void Nyan(void){
 	glClear(GL_COLOR_BUFFER_BIT);
 	
