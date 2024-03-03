29e1fe29771c33b5a7dc207eb822667c32e2b4e2
unknown <bintang.syafrian@gmail.com>
2024-03-03 23:55:25 +0700

    Menambahkan jalan raya dan awan.

--- Praktikum 2/24060122120031/tugas2.cpp
+++ Praktikum 2/24060122120031/tugas2.cpp
@@ -1,3 +1,11 @@
+/*
+    Nama: Bintang Syafrian Rizal
+    NIM : 24060122120031
+    Lab : D1
+
+    Tugas 2
+*/
+
 #include <windows.h>
 #include <gl/glut.h>
 #include <math.h>
@@ -9,6 +17,7 @@ void Display(void)
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
 
+    //Mobil F1
     glBegin(GL_POLYGON);
     glColor3f(1.0, 0, 0); 
     glVertex3f(-45.0f, 0.0f, -15.0f);
@@ -18,9 +27,8 @@ void Display(void)
     glVertex3f(-40.0f, -15.0f, -15.0f);
     glEnd();
 
-
     glBegin(GL_POLYGON);
-    glColor3f(1.0, 0, 0); // Red
+    glColor3f(1.0, 0, 0); 
     glVertex3f(-32.0f, 0.0f, 15.0f);
     glVertex3f(-5.0f, 10.0f, 15.0f);
     glVertex3f(15.0f, 10.0f, 15.0f);
@@ -43,6 +51,81 @@ void Display(void)
     glVertex3f(-30.0f, 0.0f, 15.0f);
     glVertex3f(-10.0f, 5.0f, 15.0f);
     glVertex3f(-20.0f, 5.0f, 15.0f);
+    glEnd();
+
+    //Jalan Raya
+
+    glColor3ub(255, 255, 255);
+    glBegin(GL_POLYGON);
+    glVertex2d(99000, -30);
+    glVertex2d(-2500, -30);
+    glVertex2d(-2500, -25);
+    glVertex2d(99000, -25);
+    glEnd();
+
+    glColor3ub(255, 255, 255);
+    glBegin(GL_POLYGON);
+    glVertex2d(99000, -50);
+    glVertex2d(-2500, -50);
+    glVertex2d(-2500, -45);
+    glVertex2d(99000, -45);
+    glEnd();
+
+    glColor3ub(100, 100, 100);
+    glBegin(GL_POLYGON);
+    glVertex2d(99000, -180);
+    glVertex2d(-2500, -180);
+    glVertex2d(-2500, -7);
+    glVertex2d(99000, -7);
+    glEnd();
+
+    // Awan
+    glColor3f(1.0, 1.0, 1.0); 
+    glBegin(GL_POLYGON);
+    glVertex2d(60, 70);
+    glVertex2d(90, 70);
+    glVertex2d(90, 60);
+    glVertex2d(60, 60);
+    glEnd();
+
+    glColor3f(1.0, 1.0, 1.0); 
+    glBegin(GL_POLYGON);
+    glVertex2d(70, 80);
+    glVertex2d(100, 80);
+    glVertex2d(100, 70);
+    glVertex2d(70, 70);
+    glEnd();
+
+    glColor3f(1.0, 1.0, 1.0); 
+    glBegin(GL_POLYGON);
+    glVertex2d(80, 90);
+    glVertex2d(110, 90);
+    glVertex2d(110, 80);
+    glVertex2d(80, 80);
+    glEnd();
+
+    glColor3f(1.0, 1.0, 1.0); 
+    glBegin(GL_POLYGON);
+    glVertex2d(-60, 70);
+    glVertex2d(-90, 70);
+    glVertex2d(-90, 60);
+    glVertex2d(-60, 60);
+    glEnd();
+
+    glColor3f(1.0, 1.0, 1.0); 
+    glBegin(GL_POLYGON);
+    glVertex2d(-70, 80);
+    glVertex2d(-100, 80);
+    glVertex2d(-100, 70);
+    glVertex2d(-70, 70);
+    glEnd();
+
+    glColor3f(1.0, 1.0, 1.0); 
+    glBegin(GL_POLYGON);
+    glVertex2d(-80, 60);
+    glVertex2d(-100, 60);
+    glVertex2d(-100, 50);
+    glVertex2d(-80, 50);
     glEnd();
 
     glPopMatrix();
@@ -74,6 +157,8 @@ int main(int argc, char* argv[])
     glEnable(GL_DEPTH_TEST);
     glLoadIdentity();
     glClearColor(0.529, 0.808, 0.922, 1.0);
+    glClearColor(0.529, 0.808, 0.922, 1.0);
+    glClearColor(0.529, 0.694, 0.922, 1.0);
     glOrtho (-100, 100, -100, 100,-100, 100);
     glutKeyboardFunc(SpecialKeys);
     glutDisplayFunc(Display);
